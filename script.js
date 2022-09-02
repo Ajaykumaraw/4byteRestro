7f4b28dfc6ca281de85bd3339e97170e14eddeb6
ajaykumaraw <378ajaykumar@gmail.com>
2022-09-02 19:33:31 +0530

    till review section completed

--- script.js
+++ script.js
@@ -26,3 +26,46 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   });
+
+  // review slider
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 50,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints:{
+        0:{
+          slidesPerView:1,
+        },
+        640:{
+          slidesPerView:2,
+        },
+        768:{
+          slidesPerView:2,
+        },
+        1024:{
+          slidesPerView:3,
+        },
+      }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
